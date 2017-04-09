35dfb135b931b573fa0f248468953fb5de96187a
권진구 <KwonJinGu@JinGu-MacBook-Pro.local>
2017-04-09 15:15:54 +0900

    initial Commit

---
+++ JGGraph/JGGraph/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JGGraph
+//
+//  Created by 권진구 on 2017. 4. 9..
+//  Copyright © 2017년 권진구. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
